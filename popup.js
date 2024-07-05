b9d9f6f057d093acb48f784abcdec0227ca3dcaa
rishabhknowss <rishabhrai1515@gmail.com>
2024-07-05 13:44:32 +0530

    yay

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('unfollow').addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content.js']
+    });
+  });
+  
